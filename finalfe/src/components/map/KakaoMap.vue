92d0c55860f3f5bb3a7de1fee863f290396b72e2
한재욱 <hju9707@naver.com>
2022-11-17 22:30:34 +0900

    add map marker

--- finalfe/src/components/map/KakaoMap.vue
+++ finalfe/src/components/map/KakaoMap.vue
@@ -1,7 +1,8 @@
 <template>
-  <div>
+  <div align-v="center" align-h="center" style="text-align: center">
     <h3>지도</h3>
-    <div id="map"></div>
+    <div id="map" style="margin-right: 0" align-h="center"></div>
+    <b-button @click="displayMarker(markerPositions1)">마커</b-button>
   </div>
 </template>
 
@@ -11,6 +12,12 @@ export default {
   data() {
     return {
       map: null,
+      markers: [],
+      markerPositions1: [
+        [33.452278, 126.567803],
+        [33.452671, 126.574792],
+        [33.451744, 126.572441],
+      ],
     };
   },
   methods: {
@@ -21,6 +28,47 @@ export default {
         level: 3, // 지도의 확대 레벨
       };
       this.map = new kakao.maps.Map(mapContainer, mapOption);
+    },
+    displayMarker(markerPositions) {
+      if (this.markers.length > 0) {
+        this.markers.forEach((marker) => marker.setMap(null));
+      }
+
+      const positions = markerPositions.map((position) => new kakao.maps.LatLng(...position));
+
+      if (positions.length > 0) {
+        this.markers = positions.map(
+          (position) =>
+            new kakao.maps.Marker({
+              map: this.map,
+              position,
+            })
+        );
+
+        const bounds = positions.reduce((bounds, latlng) => bounds.extend(latlng), new kakao.maps.LatLngBounds());
+
+        this.map.setBounds(bounds);
+      }
+    },
+    displayInfoWindow() {
+      if (this.infowindow && this.infowindow.getMap()) {
+        //이미 생성한 인포윈도우가 있기 때문에 지도 중심좌표를 인포윈도우 좌표로 이동시킨다.
+        this.map.setCenter(this.infowindow.getPosition());
+        return;
+      }
+
+      var iwContent = '<div style="padding:5px;">Hello World!</div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+        iwPosition = new kakao.maps.LatLng(33.450701, 126.570667), //인포윈도우 표시 위치입니다
+        iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
+
+      this.infowindow = new kakao.maps.InfoWindow({
+        map: this.map, // 인포윈도우가 표시될 지도
+        position: iwPosition,
+        content: iwContent,
+        removable: iwRemoveable,
+      });
+
+      this.map.setCenter(iwPosition);
     },
   },
   mounted() {
@@ -42,7 +90,7 @@ export default {
 
 <style scoped>
 #map {
-  width: 400px;
+  width: 100&;
   height: 500px;
 }
 </style>
