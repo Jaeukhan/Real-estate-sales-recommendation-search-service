29ba502681774459894c285022f481201b458e8f
한재욱 <hju9707@naver.com>
2022-11-18 17:04:17 +0900

    add needed board login

--- finalfe/src/router/index.js
+++ finalfe/src/router/index.js
@@ -6,28 +6,30 @@ import BoardView from "../views/BoardView.vue";
 import MemberView from "../views/MemberView.vue";
 import MapView from "../views/MapView.vue";
 
+import store from "@/store";
+
 Vue.use(VueRouter);
 
 // https://v3.router.vuejs.org/kr/guide/advanced/navigation-guards.html
-// const onlyAuthUser = async (to, from, next) => {
-//   const checkUserInfo = store.getters["memberStore/checkUserInfo"];
-//   const checkToken = store.getters["memberStore/checkToken"];
-//   let token = sessionStorage.getItem("access-token");
-//   console.log("로그인 처리 전", checkUserInfo, token);
+const onlyAuthUser = async (to, from, next) => {
+  const checkMemberInfo = store.getters["memberStore/checkMemberInfo"];
+  const checkToken = store.getters["memberStore/checkToken"];
+  let token = sessionStorage.getItem("access-token");
+  console.log("로그인 처리 전", checkMemberInfo, token);
 
-//   if (checkUserInfo != null && token) {
-//     console.log("토큰 유효성 체크하러 가자!!!!");
-//     await store.dispatch("memberStore/getUserInfo", token);
-//   }
-//   if (!checkToken || checkUserInfo === null) {
-//     alert("로그인이 필요한 페이지입니다..");
-//     // next({ name: "login" });
-//     router.push({ name: "login" });
-//   } else {
-//     console.log("로그인 했다!!!!!!!!!!!!!.");
-//     next();
-//   }
-// };
+  if (checkMemberInfo != null && token) {
+    console.log("토큰 유효성 체크하러 가자!!!!");
+    await store.dispatch("memberStore/getMemberInfo", token);
+  }
+  if (!checkToken || checkMemberInfo === null) {
+    alert("로그인이 필요한 페이지입니다..");
+    // next({ name: "login" });
+    router.push({ name: "memberLogin" });
+  } else {
+    console.log("로그인 했다!!!!!!!!!!!!!.");
+    next();
+  }
+};
 
 const routes = [
   {
@@ -49,21 +51,25 @@ const routes = [
       {
         path: "write",
         name: "boardwrite",
+        beforeEnter: onlyAuthUser,
         component: () => import("@/components/board/BoardWrite"),
       },
       {
         path: "view/:articleno",
         name: "boarddetail",
+        beforeEnter: onlyAuthUser,
         component: () => import("@/components/board/BoardDetail"),
       },
       {
         path: "modify",
         name: "boardmodify",
+        beforeEnter: onlyAuthUser,
         component: () => import("@/components/board/BoardModify"),
       },
       {
         path: "delete/:articleno",
         name: "boarddelete",
+        beforeEnter: onlyAuthUser,
         component: () => import("@/components/board/BoardDelete"),
       },
     ],
