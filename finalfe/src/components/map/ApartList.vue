628edba6e44ca007a96f445e3e6bb4bf2316db56
한재욱 <hju9707@naver.com>
2022-11-20 15:59:47 +0900

    add apartList sidbar

--- finalfe/src/components/map/ApartList.vue
+++ finalfe/src/components/map/ApartList.vue
@@ -55,6 +55,18 @@
           aria-controls="aptlist-table"
         ></b-pagination>
       </b-row>
+      <div>
+        <b-button v-b-toggle.sidebar-1>현지역 과거 부동산 동향 보기</b-button>
+        <b-sidebar id="sidebar-1" title="Sidebar" shadow>
+          <div class="px-3 py-2">
+            <p>
+              Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget
+              quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+            </p>
+            <b-img src="https://picsum.photos/500/500/?image=54" fluid thumbnail></b-img>
+          </div>
+        </b-sidebar>
+      </div>
     </b-container>
     <b-container v-else>
       <b-row style="padding: 10px">
